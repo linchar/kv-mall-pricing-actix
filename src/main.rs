d7b45cfcf4df2de86779cd355e33b9cff0db2942
linchar <linchar@gmail.com>
2024-07-16 18:51:46 -0600

    add parent and child spans

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,17 @@ use rand::Rng;
 use serde::{Deserialize, Serialize};
 use tokio::task;
 
+use opentelemetry::{
+    global,
+    trace::{FutureExt, Span, SpanKind, TraceContextExt, Tracer},
+    Context, KeyValue,
+};
+// use opentelemetry_http::{Bytes, HeaderExtractor};
+use opentelemetry_otlp::{TonicExporterBuilder, WithExportConfig};
+use opentelemetry_sdk::{
+    propagation::TraceContextPropagator, runtime::TokioCurrentThread, trace, Resource,
+};
+
 #[derive(Serialize, Debug)]
 pub struct PriceResult {
     id: i32,
@@ -15,7 +26,10 @@ impl PriceResult {
     }
 }
 
-fn get_price_from_db(id: i32) -> PriceResult {
+fn get_price_from_db(cx: Context, id: i32) -> PriceResult {
+    let tracer = global::tracer("request");
+    let _child = tracer.start_with_context("get_price_from_db", &cx);
+
     println!("getPriceFromDb thread: {:?}", std::thread::current().id());
     let mut rng = rand::thread_rng();
     let price: f64 = rng.gen_range(1.0..51.0);
@@ -29,18 +43,44 @@ struct PriceQuery {
 }
 
 async fn get_price(query: web::Query<PriceQuery>) -> Result<impl Responder> {
+    let tracer = global::tracer("request");
+
+    let parent = tracer.start("get_price");
+    let parent_cx = Context::current_with_span(parent);
+
     println!("Current thread: {:?}", std::thread::current().id());
     let id = query.id;
-    let price_result = task::spawn_blocking(move || get_price_from_db(id))
+    let price_result = task::spawn_blocking(move || get_price_from_db(parent_cx, id))
         .await
         .map_err(|e| actix_web::error::ErrorInternalServerError(e))?;
+
     Ok(web::Json(price_result))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Start a new OTLP trace pipeline
+    global::set_text_map_propagator(TraceContextPropagator::new());
+
+    let service_name_resource = Resource::new(vec![KeyValue::new(
+        opentelemetry_semantic_conventions::resource::SERVICE_NAME,
+        "pricing_rust",
+    )]);
+
+    let _tracer: trace::Tracer = opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(TonicExporterBuilder::default().with_endpoint("http://localhost:4317"))
+        .with_trace_config(trace::Config::default().with_resource(service_name_resource))
+        .install_batch(TokioCurrentThread)
+        .expect("pipeline install error");
+
     HttpServer::new(|| App::new().service(web::resource("/price").route(web::get().to(get_price))))
         .bind("127.0.0.1:8080")?
         .run()
-        .await
+        .await?;
+
+    // Ensure all spans have been reported
+    global::shutdown_tracer_provider();
+
+    Ok(())
 }
